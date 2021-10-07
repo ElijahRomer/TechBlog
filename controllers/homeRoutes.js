fae9d7065d60342923708d59c921e5b4b62c5091
Elijah Romer <romere88@gmail.com>
2021-10-07 10:08:44 -0500

    split up login and createAccount views

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,15 @@ const router = require(`express`).Router();
 const withAuth = require(`../utils/auth`);
 
 router.get(`/`, (req, res) => {
+  res.render(`homepage`);
+});
+
+router.get(`/dashboard`, withAuth, (req, res) => {
   res.render(`dashboard`);
-})
+});
+
+router.get(`/loginCreateAccount`, withAuth, (req, res) => {
+  res.render(`loginCreateAccount`);
+});
 
 module.exports = router;
