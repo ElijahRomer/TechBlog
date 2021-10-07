0f4d3c24638dfa3a47d38d18a39e3efe3074a9ad
Elijah Romer <romere88@gmail.com>
2021-10-07 15:54:47 -0500

    wrote client side script for login page, and corresponding route. Login is now functional and withAuth checks pass when logged in.

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -1,6 +1,39 @@
 const router = require(`express`).Router();
 const withAuth = require(`../../utils/auth`);
 const { Comment, Post, User } = require(`../../models`)
-// ROUTES HERE
+
+router.post(`/login`, async (req, res) => {
+  console.log(`/login ROUTE SLAPPED`)
+  console.log(req.body)
+  try {
+    const userData = await User.findOne({ where: { user_name: req.body.user_name } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    };
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ message: `Login attempt successful!` })
+    })
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+})
 
 module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,6 @@ const router = require(`express`).Router();
 
 const apiRoutes = require(`./apiRoutes`);
 
-router.use('/api', apiRoutes);
+router.use('/', apiRoutes);
 
 module.exports = router;
