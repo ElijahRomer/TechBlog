02312d644de4e6cf8a9c825282e63cf06c048045
Elijah Romer <romere88@gmail.com>
2021-10-08 09:42:34 -0500

    enabled conditional rendering of login/ logout buttons in handlebars depending on if the user is logged in or not.

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -3,14 +3,29 @@ const withAuth = require(`../utils/auth`);
 
 
 router.get(`/dashboard`, withAuth, (req, res) => {
-  res.render(`dashboard`);
+  console.log(`/dashboard ROUTE SLAPPED`)
+  console.log(req.session)
+
+  res.render(`dashboard`, {
+    logged_in: req.session.logged_in
+  });
 });
 
 router.get(`/login`, (req, res) => {
-  res.render(`login`);
+  console.log(`/login ROUTE SLAPPED`)
+  console.log(req.session)
+
+  res.render(`login`, {
+    logged_in: req.session.logged_in
+  });
 });
 
 router.get(`/`, (req, res) => {
-  res.render(`homepage`);
+  console.log(`/ ROUTE SLAPPED`)
+  console.log(req.session)
+
+  res.render(`homepage`, {
+    logged_in: req.session.logged_in
+  });
 });
 module.exports = router;
