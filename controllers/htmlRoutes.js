2bd0edc14c871fd2b542194c4d30d55b13aee5a9
Elijah Romer <romere88@gmail.com>
2021-10-11 17:17:18 -0500

    set up dashboard route to query data on user and their posts, and deliver it to handlebars for dynamic rendering. Adjusted the posts bootstrap accordion to dynamically render unique html attributes for each rendered entry so that the controls will work independently of each other.

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -5,12 +5,26 @@ const { Comment, Post, User } = require(`../models`)
 router.get(`/dashboard`, withAuth, async (req, res) => {
   try {
     console.log(`/dashboard ROUTE SLAPPED`)
-    console.log(req.session)
+    console.log(req.session);
 
     // make a call for post data to render in response
+    let rawUserPostData = await Post.findAll({
+      where: {
+        creator_id: req.session.user_id
+      },
+      include: [{ model: User }]
+    });
+
+
+    const userPosts = rawUserPostData.map((post) => post.get({ plain: true }));
+
+    console.log(userPosts);
+
+
 
     res.render(`dashboard`, {
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
+      userPosts,
     });
   } catch (err) {
     console.log(err);
